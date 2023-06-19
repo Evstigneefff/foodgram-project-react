24ae1f07026ed5284fcb185bb4153fbcd7f238bf
Алексей <evstigneefff@gmail.com>
2023-06-19 23:45:01 +0300

    fix_bd_3

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -4,8 +4,8 @@ from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'tags', TagViewSet, basename='tags')
-router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router.register(r'api/tags', TagViewSet, basename='tags')
+router.register(r'api/ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'', RecipeViewSet, basename='recipes')
 
 urlpatterns = router.urls
--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import routers
+
+from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'api/tags', TagViewSet, basename='tags')
+router.register(r'api/ingredients', IngredientViewSet, basename='ingredients')
+router.register(r'api/recipes/', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('api/recipes/', include('api.urls')),
     path('api/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+urlpatterns += router.urls

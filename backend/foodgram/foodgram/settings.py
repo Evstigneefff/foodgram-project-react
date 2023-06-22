8ebd19cebac29181e7e7278d45135cf7038e2008
Алексей <evstigneefff@gmail.com>
2023-06-22 20:38:05 +0300

    rew_fix1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.68.90', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = os.getenv('SECRET_KEY').split
 
 
 INSTALLED_APPS = [
